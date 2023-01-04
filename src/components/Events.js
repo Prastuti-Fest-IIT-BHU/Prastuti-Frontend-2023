bef3ea9e15978abc16e6ca7e2e87e2c5a0af3bb8
raiharsh1811 <harsh.rai.eee21@iitbhu.ac.in>
2023-01-04 23:04:42 +0530

    new animation hangs

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,8 +5,10 @@ import PropTypes from "prop-types";
 import { motion, useScroll, useSpring } from "framer-motion";
 
 const boxVariant = {
-  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-  hidden: { opacity: 0, scale: 0 },
+  visible: {x: '0%',transition: { delay: 0, type: "spring", stiffness: 30} },
+  hidden: {  x: '100%' }// new animation
+  // visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  // hidden: { opacity: 0, scale: 0 }, old animation
 };
 const Card = (prop) => {
   const { scrollYProgress } = useScroll();
@@ -15,12 +17,14 @@ const Card = (prop) => {
     damping: 30,
     restDelta: 0.001,
   });
+
+
   return (
     <div style={{background:' linear-gradient(90deg, rgba(219,242,255,1) 0%, rgba(185,250,255,1) 100%)'}}>
     {Data.map((item, index) => (
     <>
     
-        <motion.div
+        <motion.div 
           class="pt-2 pb-12 bg-gray-300"
           variants={boxVariant}
           initial="hidden"
