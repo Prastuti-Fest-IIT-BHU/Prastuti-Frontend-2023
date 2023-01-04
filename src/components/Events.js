fcbed535d3c4b976972008876ec28a3c5970edcc
raiharsh1811 <harsh.rai.eee21@iitbhu.ac.in>
2023-01-05 00:14:56 +0530

    older animation

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Data } from './Data';
-
+import './Event.css';
 import PropTypes from "prop-types";
 import { motion, useScroll, useSpring } from "framer-motion";
 
 const boxVariant = {
-  visible: {x: '0%',transition: { delay: 0, type: "spring", stiffness: 30} },
-  hidden: {  x: '100%' }// new animation
-  // visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-  // hidden: { opacity: 0, scale: 0 }, old animation
+   visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+   hidden: { opacity: 0, scale: 0 }
 };
 const Card = (prop) => {
   const { scrollYProgress } = useScroll();
@@ -20,12 +18,13 @@ const Card = (prop) => {
 
 
   return (
-    <div style={{background:' linear-gradient(90deg, rgba(219,242,255,1) 0%, rgba(185,250,255,1) 100%)'}}>
+    <div className="animate-slide" style={{background:' linear-gradient(90deg, rgba(219,242,255,1) 0%, rgba(185,250,255,1) 100%)'}}>
     {Data.map((item, index) => (
     <>
     
         <motion.div 
-          class="pt-2 pb-12 bg-gray-300"
+          class="pt-2 pb-12 bg-gray-300 x"
+
           variants={boxVariant}
           initial="hidden"
           whileInView={"visible"}
