8bff36061100504f8009ec84c416f8b1c22a9f61
1eque <vanithapar17@gmail.com>
2023-01-04 15:48:59 +0530

    added faq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Register from "./components/Register";
 import Teams from "./components/Teams";
+import Faq from '././components/Faq'
 
 export default function App() {
   return (
     <>
       <Register />
       <Teams />
+      <Faq/>
     </>
   )
 }
