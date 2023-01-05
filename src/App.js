9a83f9ae1aa3454b8faa2c1d1abd30c2ddec1f30
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-01-06 01:11:28 +0530

    changed login and teams page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './components/About'
 import Separate_Event from './components/Separate_Event';
 import eventData from './components/Eventdata';
 
-import {BrowserRouter as Router , Routes ,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import PrastutiTeam from "./components/Team/Teams"
@@ -14,37 +14,36 @@ import Intro from "./components/Intro";
 
 export default function App() {
   return (
-    <>   
-    <Router>
-    <div className="sticky top-0 z-20">
-            <Navbar/>
-    </div>
-      <Routes>
-        <Route path='/' element={
-          <>
-            <Intro />
-            <Register />
-          <Teams />
-          <Faq/>
-          <Sponsor/>
-          <About/>
-          <Footer/>
-          </>
-        }></Route>
-        <Route
-            path="/register"
-            element={<Register/>}
-          ></Route>
-        <Route path='/codigo' element={<Separate_Event data={eventData.codigo}/>}></Route>
-        <Route path='/hackoverflow' element={<Separate_Event data={eventData.hackoverflow} />}></Route>
-        <Route path='/recognizance' element={<Separate_Event data={eventData.recognizance} />}></Route>
-        <Route path='/simulim' element={<Separate_Event data={eventData.simulim} />}></Route>
-        <Route path='/consilium' element={<Separate_Event data={eventData.consilium} />}></Route>
-        <Route path='/oscillion' element={<Separate_Event data={eventData.oscillion} />}></Route>
-        <Route path='/login' element={<Register/>}></Route>
-        <Route path='/team' element={<PrastutiTeam/>}></Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <div className="sticky top-0 z-20">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path='/' element={
+            <>
+              <Intro />
+              <Register />
+            </>
+          }>
+
+          </Route>
+          <Route path='/teams' element={<Teams />}></Route>
+          <Route path='/faq' element={<Faq />}></Route>
+          <Route path='/sponsors' element={<Sponsor />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path='/codigo' element={<Separate_Event data={eventData.codigo} />}></Route>
+          <Route path='/hackoverflow' element={<Separate_Event data={eventData.hackoverflow} />}></Route>
+          <Route path='/recognizance' element={<Separate_Event data={eventData.recognizance} />}></Route>
+          <Route path='/simulim' element={<Separate_Event data={eventData.simulim} />}></Route>
+          <Route path='/consilium' element={<Separate_Event data={eventData.consilium} />}></Route>
+          <Route path='/oscillion' element={<Separate_Event data={eventData.oscillion} />}></Route>
+          <Route path='/login' element={<Register />}></Route>
+          <Route path='/team' element={<PrastutiTeam />}></Route>
+        </Routes>
+      </Router>
+      <Footer />
     </>
   );
 }
