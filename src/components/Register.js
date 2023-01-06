46ba5065710622e6c8f3ee7e2b6b6b8a12fd3f1e
nilaymundra <90716872+nilaymundra@users.noreply.github.com>
2023-01-07 00:57:14 +0530

    Fixed Horizontal Scroll and Mobile Compatibility

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,17 +3,16 @@ import image from "../assets/Register/logo.png";
 import style from "../assets/Register/style.css"
 const Register = () => {
   return (
-    <div className="main relative bg-no-repeat bg-cover w-screen h-screen light-gradient bg-center">
-      <span className="absolute flex align-middle justify-center h-screen w-screen">
+    <div className="main relative bg-no-repeat bg-cover h-screen light-gradient bg-center">
+      <span className="absolute flex align-middle justify-center h-screen">
         <svg class="blob absolute justify-center align-middle" width={900} height={900} viewBox="0 0 450 450" xmlns="http://www.w3.org/2000/svg">
           <g transform="translate(225,250)">
             <path fill="#003247" d="M120,-157.6C152.7,-141.5,174.3,-102.6,194.8,-58.8C215.3,-14.9,234.6,33.8,228.4,80.8C222.2,127.8,190.4,173.1,148.1,184C105.8,195,52.9,171.5,-2.4,174.8C-57.8,178.2,-115.6,208.4,-137.5,190.9C-159.3,173.3,-145.3,108,-153,56.3C-160.7,4.6,-190.2,-33.4,-178.3,-54.2C-166.4,-75.1,-113.2,-78.8,-76.6,-93.6C-40,-108.3,-20,-134.2,11.9,-150.5C43.7,-166.8,87.4,-173.6,120,-157.6Z" />
           </g>
         </svg>
       </span >
-      <div className="flex flex-col items-center justify-center h-screen w-screen">
-        <div className="absolute z-10 flex h-[37rem] w-[30rem] max-w-sm  mt-30   bg-[#DBF2FF] mx-auto shadow-2xl shadow-dark-neon-tint rounded-lg bg-opacity-65 lg:max-w-4xl">
-          <div className="absolute w-full content-center p-6 md:px-8 ">
+      <div className="flex flex-col items-center justify-center h-screen">
+          <div className="absolute z-10 flex h-[37rem] w-[22rem] sm:w-[30rem] mt-30 bg-[#DBF2FF] mx-auto shadow-2xl shadow-dark-neon-tint rounded-lg bg-opacity-65 lg:max-w-4xl">          <div className="absolute w-full content-center p-6 md:px-8 ">
             <h2 className="text-2xl font-semibold text-center text-gray-700">
               Brand
             </h2>
