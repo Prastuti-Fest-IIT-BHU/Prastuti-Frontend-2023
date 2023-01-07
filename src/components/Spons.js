ee4eed8e96ec537f82aa5bdeefed54cb05abf0e0
sumant7 <91867296+sumant7@users.noreply.github.com>
2023-01-08 03:08:47 +0530

    Spons links

--- src/components/Spons.js
+++ src/components/Spons.js
@@ -9,17 +9,24 @@ export default function Spons() {
             <div className="line">
                 <div className="sponscard">
                     <h1 className='cardtitle'> Co-Title Sponsor</h1>
-                    <img src="https://www.skillreactor.io/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fsrc%2Fassets%2Fimages%2Fskillreactor-logo.91718bff253ba0c12b577cd882b48c17.svg&w=256&q=75" className='skill' alt="" />
+                    <a href="https://www.skillreactor.io/" target="_blank" rel="noreferrer noopener">
+                        <img src="https://www.skillreactor.io/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fsrc%2Fassets%2Fimages%2Fskillreactor-logo.91718bff253ba0c12b577cd882b48c17.svg&w=256&q=75" className='skill' alt="" />
+                    </a>
                 </div>
             </div>
             <div className="line">
                 <div className="sponscard">
                     <h1 className='cardtitle'> Research Partner</h1>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png" alt="" className='ieee' />
+                    <a href="https://www.ieee.org/" target="_blank" rel="noreferrer noopener">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png" alt="" className='ieee' />
+                    </a>
                 </div>
                 <div className="sponscard">
                     <h1 className='cardtitle'> Education Partner</h1>
-                    <img src="https://techanalogy.org/_next/image?url=%2Ficon.svg&w=256&q=75" alt="" className='tech' />
+                    <a href="https://techanalogy.org/" target="_blank" rel="noreferrer noopener">
+                        <img src="https://techanalogy.org/_next/image?url=%2Ficon.svg&w=256&q=75" alt="" className='tech' />
+                    </a>
+
                 </div>
             </div>
 
