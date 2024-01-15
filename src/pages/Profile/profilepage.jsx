fc736cc506005a4cd3db06c199b38280af3629b2
prastutitech <97397406+prastutitech@users.noreply.github.com>
2024-01-15 14:57:26 +0530

    Update profilepage.jsx

--- src/pages/Profile/profilepage.jsx
+++ src/pages/Profile/profilepage.jsx
@@ -7,7 +7,6 @@ import Footer from "../../components/Footer/Footer";
 import axios from "axios";
 import Loader from "../../components/Loader/loader";
 
-
 const Profilepage = () => {
   const [addclass, setaddclass] = useState(["onclicknav", "", ""]);
   const [team, setteam] = useState(null);
@@ -19,7 +18,6 @@ const Profilepage = () => {
   const [showLoader, setShowLoader] = useState(false);
   const [loaderText, setLoaderText] = useState("");
   const [eventData, setEventData] = useState(null);
-
 
   const showLoaderWithMessage = (message) => {
     setLoaderText(message);
@@ -48,23 +46,22 @@ const Profilepage = () => {
     if (localStorage.getItem("loginData")) {
       const gettingData = async () => {
         showLoaderWithMessage("Fetching Details");
+        // console.log(localStorage.getItem("loginData"));
         const { data } = await axios.get(
-          `${process.env.REACT_APP_SECRET_KEY}/api/user/${localStorage.getItem(
-            "loginData"
-          )}`
+          `http://localhost:3000/api/user/${localStorage.getItem("loginData")}`
         );
+        console.log(data);
 
-        const datas = await axios.get(
-          `${process.env.REACT_APP_SECRET_KEY}/api/events`
-        );
-          
-
+        const datas = await axios.get(`http://localhost:3000/api/events`);
+        // console.log(datas.data);
         let links = {};
 
         datas.data.events.map((data, index) => {
+          // console.log(data);
           links[data.Name] = data.whatsappLink;
-        })
-        
+        });
+        // console.log(links);
+
         setEventData(links);
 
         setinput(data[0]);
@@ -75,7 +72,7 @@ const Profilepage = () => {
           <Profileevent
             event={data[0].Events_Participated}
             team={data[0].Teams}
-            waLink = {links}
+            waLink={links}
           />
         );
         setrequest(data[0].Pending_Requests);
@@ -109,8 +106,8 @@ const Profilepage = () => {
           >
             <div className="min-w-full md:rounded-b-xl h-[160px] -mb-4 bg-black/60"></div>
           </div>
-          {/* <img src={profileback} className=" relative -mb-36 object-cover opacity-90" /> */}
-          {/* <div className="myprofile -mt-12 flex flex-col md:flex-row">
+
+          <div className="myprofile -mt-12 flex flex-col md:flex-row">
             <div className="profilechild imgprofile">
               <img
                 className="imgprofile"
@@ -126,12 +123,11 @@ const Profilepage = () => {
                 {input.email_id}
               </div>
             </div>
-          </div> */}
-
+          </div>
         </div>
         <div></div>
         <div className="pb-2 m-2 flex flex-col lg:items-center lg:flex-row lg:px-10">
-          {/* <div className="about-user lg:min-h-[40vh] m-2 p-2 bg-sky-50 rounded-2xl flex flex-col lg:w-4/12">
+          <div className="about-user lg:min-h-[40vh] m-2 p-2 bg-sky-50 rounded-2xl flex flex-col lg:w-4/12">
             <p className="about-header text-center">About</p>
             <hr className="Phr" />
             <div className="user-details">
@@ -156,9 +152,9 @@ const Profilepage = () => {
                 Sign Out
               </button>
             </div>
-          </div> */}
+          </div>
 
-          <div className="lg:w-full lg:max-w-md px-8 py-4 mt-16 mb-16 bg-white rounded-lg shadow-lg md:justify-center">
+          {/* <div className="lg:w-full lg:max-w-md px-8 py-4 mt-16 mb-16 bg-white rounded-lg shadow-lg md:justify-center">
             <div className="flex justify-center m-1 mb-4 md:justify-start">
               <img
                 className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full"
@@ -174,15 +170,11 @@ const Profilepage = () => {
             <h2 className="mt-2 text-2xl text-center font-Manrope font-semibold text-gray-800 md:mt-0 md:text-3xl">
               About
             </h2>
-            <p className=" mt-2 font-Nunito text-gray-600 ">
-              {input.email_id}
-            </p>
+            <p className=" mt-2 font-Nunito text-gray-600 ">{input.email_id}</p>
             <p className=" profile-contact mt-2 font-Nunito text-gray-600">
               +91{input.Phone}
             </p>
-            <p className=" mt-2 font-Nunito  text-gray-600">
-              {input.College}
-            </p>
+            <p className=" mt-2 font-Nunito  text-gray-600">{input.College}</p>
 
             <div className="flex justify-end mt-4">
               <div className="sign-out-btn  flex justify-center">
@@ -197,14 +189,20 @@ const Profilepage = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
 
           <div className="dynamic-content w-[100%] sm:m-2 min-h-[60vh] bg-sky-50 rounded-2xl  lg:w-8/12">
             <div className="profilenav flex">
               <div
                 className={`Pnavchild px-4 ${addclass[0]}`}
                 onClick={() => {
-                  setvalue(<Profileevent event={event} team={team} waLink={eventData}/>);
+                  setvalue(
+                    <Profileevent
+                      event={event}
+                      team={team}
+                      waLink={eventData}
+                    />
+                  );
                   handleevent(0);
                 }}
               >
